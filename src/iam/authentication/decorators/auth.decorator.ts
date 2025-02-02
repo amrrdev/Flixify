a137623d9c2a006d596efce6c8255ba2998d2c17
Amr Mubarak <amrrdev@gmail.com>
2025-02-02 14:43:09 +0200

    feat(auth): add auth decorator for setting authentication type metadata

---
+++ src/iam/authentication/decorators/auth.decorator.ts
@@ -0,0 +1,6 @@
+import { SetMetadata } from '@nestjs/common';
+import { AuthType } from '../enum/auth-type.enum';
+import { AUTH_TYPE_KEY } from '../iam.constants';
+
+export const Auth = (authType: AuthType | AuthType[]) =>
+  SetMetadata(AUTH_TYPE_KEY, authType);

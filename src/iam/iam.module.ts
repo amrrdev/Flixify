24f610ba49d82a5b32b160188703f58c65d8178c
Amr Mubarak <amrrdev@gmail.com>
2025-02-02 14:44:15 +0200

    feat(module): configure IAM module with authentication and hashing services

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -5,17 +5,33 @@ import { DatabaseModule } from '../database/database.module';
 import { AuthenticationService } from './authentication/authentication.service';
 import { AuthenticationController } from './authentication/authentication.controller';
 import { DatabaseService } from '../database/database.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './authentication/config/jwt.config';
+import { AuthenticationGuard } from './authentication/guards/authentication.guard';
+import { AccessTokenGuard } from './authentication/guards/access-token.guard';
+import { APP_GUARD, Reflector } from '@nestjs/core';
 
 @Module({
+  imports: [
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    DatabaseModule,
+  ],
   controllers: [AuthenticationController],
   providers: [
-    DatabaseModule,
     {
       provide: HashingService,
       useClass: BcryptService,
     },
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticationGuard,
+    },
+    AccessTokenGuard,
     AuthenticationService,
     DatabaseService,
+    Reflector,
   ],
 })
 export class IamModule {}

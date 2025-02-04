0525cc9a1e44b99a52a5a2927086a64dbf0b498c
Amr Mubarak <amrrdev@gmail.com>
2025-02-04 23:00:05 +0200

    Create ActiveUser parameter decorator to extract user data from request

---
+++ src/iam/decorators/active-user.decorator.ts
@@ -0,0 +1,13 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
+import { REQUEST_USER_KEY } from '../iam.constants';
+import { ActiveUserDate } from '../interfaces/active-user-data.interface';
+
+export const ActiveUser = createParamDecorator(
+  (field: keyof ActiveUserDate, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest<Request>();
+    const user: ActiveUserDate | undefined = request[REQUEST_USER_KEY];
+
+    return field ? user?.[field] : user;
+  },
+);
